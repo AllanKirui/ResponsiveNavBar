2d42868e4e996f20b47abe1f517a619f78d85bc5
Allan Kirui <nallawilljr@gmail.com>
2021-02-04 22:37:52 +0300

    Added burger functionality

--- static/js/script.js
+++ static/js/script.js
@@ -20,8 +20,21 @@ read_more_btn.addEventListener("click", (e)=>{
 // Get the burger and nav otions class
 const burger = document.querySelector(".burger");
 const nav = document.querySelector(".nav-options");
+const nav_options = document.querySelectorAll(".nav-options li");
 
-// Listen for a click event
+// Listen for a click event, and toggle nav
 burger.addEventListener("click", (e)=>{
     nav.classList.toggle("nav-active");
-})
+
+    // Animate links
+    nav_options.forEach((link, index) => {
+        if (link.style.animation){
+            link.style.animation = "";
+        } else {
+            link.style.animation = `nav-option-fade 0.5s ease forwards ${index / 7 + 0.3}s`;
+        }
+    });
+
+    // Burger animation
+    burger.classList.toggle("toggle");
+});
